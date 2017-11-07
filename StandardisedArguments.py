bac31dd239d6c59bac3c954e9144eea29e801e4b
Guy Sherman <guysherman@Guys-MacBook-Pro-3.local>
2017-11-07 22:21:52 +0000

    Added required flags to all arguments
    The point of this structure is to only add the sets of arguments
    if they are desired. -- arguments are used to avoid ordering issues,
    but we still want them all to be required

--- StandardisedArguments.py
+++ StandardisedArguments.py
@@ -13,8 +13,8 @@ def create_basic_parser(description):
     Return value:- an argparse parser object
     """
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('--out', nargs=1, help="The directory to output results to.")
-    parser.add_argument('--in', nargs='+', action='append', help="A path to process.")
+    parser.add_argument('--out', nargs=1, required=True, help="The directory to output results to.")
+    parser.add_argument('--input', nargs='+', required=True, help="A path to process.")
     return parser
 
 
@@ -25,10 +25,14 @@ def add_api_arguments(parser):
     parser:- an argparse parser object to add the arguments to
     Return value:- the parser object that was passed in
     """
-    parser.add_argument('--api_url', nargs=1, help="The base api url to use.")
-    parser.add_argument('--api_key', nargs=1, help="The token, or key, to send with api calls.")
+    parser.add_argument('--api_url', nargs=1, required=True, help="The base api url to use.")
+    parser.add_argument('--api_key',
+                        nargs=1,
+                        required=True,
+                        help="The token, or key, to send with api calls.")
     parser.add_argument('--api_key_header',
                         nargs=1,
+                        required=True,
                         help="The header name to send the key as, eg x-token, or Authorization")
     return parser
 
@@ -39,10 +43,10 @@ def add_sql_server_arguments(parser):
     parser:- an argparse parser object to add the arguments to
     Return value:- the parser object that was passed in
     """
-    parser.add_argument('--sql_server', nargs=1, help='the sql server to connect to')
-    parser.add_argument('--sql_username', nargs=1, help='the user to connect as')
-    parser.add_argument('--sql_password', nargs=1, help='the password to use')
-    parser.add_argument('--sql_database', nargs=1, help='the db to access')
+    parser.add_argument('--sql_server', nargs=1, required=True, help='the sql server to connect to')
+    parser.add_argument('--sql_username', nargs=1, required=True, help='the user to connect as')
+    parser.add_argument('--sql_password', nargs=1, required=True, help='the password to use')
+    parser.add_argument('--sql_database', nargs=1, required=True, help='the db to access')
     return parser
 
 def add_crypto_arguments(parser):
@@ -52,7 +56,7 @@ def add_crypto_arguments(parser):
     parser:- an argparse parser object to add the arguments to
     Return value:- the parser object that was passed in
     """
-    parser.add_argument('--crypto_key', nargs=1, help='The encryption key to use')
+    parser.add_argument('--crypto_key', nargs=1, required=True, help='The encryption key to use')
     return parser
 
 def add_azure_storage_arguments(parser):
@@ -62,7 +66,16 @@ def add_azure_storage_arguments(parser):
     parser:- an argparse parser object to add the arguments to
     Return value:- the parser object that was passed in
     """
-    parser.add_argument('--storage_account_name', nargs=1, help='the storage account name')
-    parser.add_argument('--storage_account_key', nargs=1, help='the key to access the account')
-    parser.add_argument('--storage_container_name', nargs=1, help='the container to sync')
+    parser.add_argument('--storage_account_name',
+                        nargs=1,
+                        required=True,
+                        help='the storage account name')
+    parser.add_argument('--storage_account_key',
+                        nargs=1,
+                        required=True,
+                        help='the key to access the account')
+    parser.add_argument('--storage_container_name',
+                        nargs=1,
+                        required=True,
+                        help='the container to sync')
     return parser
